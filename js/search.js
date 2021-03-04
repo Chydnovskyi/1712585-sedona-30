a83e571d9907fc28bd4381adece91471339f3d75
Chydnovskyi <ChydnovskyiMV@ya.ru>
2021-03-04 21:37:47 +0300

    12
    не могу разобраться в js завтра попробую

---
+++ js/search.js
@@ -0,0 +1,12 @@
+const search = docunent.querySelector(".search");
+const searchPopup = document.querySelector(".interest-search");
+
+search.addEventListener("click", function () {
+    evt.preventDefault();
+    searchPopup.classList.add ("interest-search-show");
+});
+
+search.addEventListener("click", function () {
+    evt.preventDefault();
+    searchPopup.classList.remove ("interest-search-show");
+});
