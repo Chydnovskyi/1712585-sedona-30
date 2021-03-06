01a4f15d66c4e5335f5b217d7d095b2b2a4eda9e
Chydnovskyi <ChydnovskyiMV@ya.ru>
2021-03-06 18:01:38 +0300

    13

--- js/search.js
+++ js/search.js
@@ -3,10 +3,10 @@ const searchPopup = document.querySelector(".interest-search");
 
 search.addEventListener("click", function () {
     evt.preventDefault();
-    searchPopup.classList.add ("interest-search-show");
+    searchPopup.classList.add("interest-search-show");
 });
 
 search.addEventListener("click", function () {
     evt.preventDefault();
-    searchPopup.classList.remove ("interest-search-show");
+    searchPopup.classList.remove("interest-search-show");
 });
