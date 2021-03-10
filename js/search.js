9345923bf11ca5d019b16fff871cc62ecbe8a845
Chydnovskyi <ChydnovskyiMV@ya.ru>
2021-03-10 22:58:21 +0300

    16

--- js/search.js
+++ js/search.js
@@ -1,21 +1,29 @@
 const search = document.querySelector(".search");
 const searchPopup = document.querySelector(".interest-search");
-const interest_form = document.querySelector("interest-form");
-const adults = document.querySelector(".adults");
-const children = document.querySelector(".children");
+const search_button = document.querySelector(".search_button");
+const check = document.querySelector(".check");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("interest");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 search.addEventListener("click", function (evt) {
     evt.preventDefault();
     searchPopup.classList.toggle("interest-search-show");
 });
 
-interest_form.addEventListener("submit", function (evt) {
-    if (!adults.value || !children.value) {
+search_button.addEventListener("submit", function (evt) {
+    if (!check.value) {
       evt.preventDefault();
       loginPopup.classList.add("modal-error");
     } else {
       if (isStorageSupport) {
-        localStorage.setItem("login", loginLogin.value);
+        localStorage.setItem("interest", loginLogin.value);
       }
     }
   });
